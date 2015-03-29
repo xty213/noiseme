a1727fc60d88b392c2de34bf56cd6d5f162d2975
Tianyu Xu <xty213@gmail.com>
2015-03-29 00:05:59 -0400

    refactor

--- find_correlation.py
+++ find_correlation.py
@@ -15,8 +15,6 @@ IMAGE_LABEL_SCORE_THRESHOLD = 0.1
 #######################################
 #              VARIABLES              #
 #######################################
-correlation = {}
-
 curr_video_id = None
 curr_noiseme_lines = None
 noiseme_succ = False
@@ -35,47 +33,48 @@ for image_label_file_path in IMAGE_LABEL_FILES:
             video_id = keyframe_info[0]
             timestamp = keyframe_info[1]
 
+            # update the noiseme cache if necessary
+            if curr_video_id != video_id:
+                curr_video_id = video_id
+                try:
+                    f = open(AUDIO_LABEL_FOLDER + video_id + '.txt', 'r')
+                except IOError:
+                    sys.stderr.write('No audio labels: %s\n' % video_id)
+                    noiseme_succ = False
+                    continue
+                else:
+                    curr_noiseme_lines = f.readlines()
+                    noiseme_succ = True
+                    f.close()
+            elif not noiseme_succ:
+                continue
+
+            # compute the target time frame
+            # every 0.1 second is a time frame
+            time_arr = timestamp.split('^')
+            minute = time_arr[0]
+            second = time_arr[1][:-4] # '03.033.jpg'[:-4] = '03.033'
+            line_num = int(minute) * 600 + int(float(second) * 10)
+
+            # get noiseme info in that time frame
+            try:
+                audio_info = curr_noiseme_lines[line_num].split(' ')
+            except IndexError:
+                sys.stderr.write('%s have:%d try:%d\n' % (video_id, len(curr_noiseme_lines), line_num))
+
+            # find positive noiseme labels in target time frame
+            labels = {}
+            for i in xrange(len(audio_info)):
+                noiseme_and_score = audio_info[i].split(':')
+                if noiseme_and_score[0][0] == '1': # '1.0'[0] = '1'
+                    labels[i + 1] = float(noiseme_and_score[1])
+
             for i in xrange(1, len(str_arr)):
                 concept_info = str_arr[i].split(':')
                 concept_id = int(concept_info[0])
                 concept_conf = float(concept_info[1])
-                # if we see some image concept, try to read noiseme labels
+                # if we see some image concept
                 if concept_conf >= IMAGE_LABEL_SCORE_THRESHOLD:
-                    # compute the target time frame
-                    # every 0.1 second is a time frame
-                    time_arr = timestamp.split('^')
-                    minute = time_arr[0]
-                    second = time_arr[1][:-4] # '03.033.jpg'[:-4] = '03.033'
-                    line_num = int(minute) * 600 + int(float(second) * 10)
-
-                    # get noiseme info in that time frame
-                    if curr_video_id != video_id:
-                        curr_video_id = video_id
-                        try:
-                            f = open(AUDIO_LABEL_FOLDER + video_id + '.txt', 'r')
-                        except IOError:
-                            sys.stderr.write('No audio labels: %s\n' % video_id)
-                            noiseme_succ = False
-                            continue
-                        else:
-                            curr_noiseme_lines = f.readlines()
-                            noiseme_succ = True
-                            f.close()
-                    if noiseme_succ:
-                        try:
-                            audio_info = curr_noiseme_lines[line_num].split(' ')
-                        except IndexError:
-                            sys.stderr.write('%s have:%d try:%d\n' % (video_id, len(curr_noiseme_lines), line_num))
-                    else:
-                        continue
-
-                    # find the positive noiseme labels
-                    labels = {}
-                    for i in xrange(len(audio_info)):
-                        noiseme_and_score = audio_info[i].split(':')
-                        if noiseme_and_score[0][0] == '1': # '1.0'[0] = '1'
-                            labels[i + 1] = float(noiseme_and_score[1])
-
                     for label, noiseme_score in labels.items():
                         print '%d:%d\t%f' % (concept_id, label, noiseme_score * concept_conf)
     finally:
